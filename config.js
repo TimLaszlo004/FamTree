398741c6ebe9265a895307408636d2925cba1fdd
TimLaszlo004 <laszlo.tim0112@gmail.com>
2026-01-18 13:09:00 +0100

    add valid configs

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4O5x8OEkTC7qhGRV_SUo-oHR1Oo7TLKI",
+  authDomain: "famtree-e7f51.firebaseapp.com",
+  projectId: "famtree-e7f51",
+  storageBucket: "famtree-e7f51.firebasestorage.app",
+  messagingSenderId: "671703137374",
+  appId: "1:671703137374:web:5e022f7b2e97192bf9c8cf",
+  measurementId: "G-EKCCH17KTY",
+};
